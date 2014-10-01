b1a968b87e93016791043b53cadb4470f36d006c
John Crocker <johnniecrocker@yahoo.com>
2014-10-01 16:09:11 -0400

    Got half of question 1, question two complete

--- main.js
+++ main.js
@@ -11,21 +11,62 @@ var answer1,
     eightitems,           //items with more than 8 materials
     gbpitems,             //items denominated in pounds
     midrangeitems,        //items priced between $14 and $18
+    price,
     selfmadeitems,        //items made by seller
     wooditems             //items made of wood
     ;
 
 //This is question 1
 
+    var pricearray = items.map(function(itemprice){
+
+                                          return itemprice.price
+
+                                        });
+
+    var numitems = pricearray.length;
+
+    var sum = function(){
+
+              price
+
+              }
+
+
+
     answer1 = avgprice;
     console.log(avgprice);
 
 //This is question 2
 
+    c
+c
+    midrangeitems = midrangeitemsfourteen.filter(function(miditems) {
+
+                            if (miditems.price <= 18) {
+
+                                    return miditems};
+                                  });
+
+
     console.log(midrangeitems);
     answer2 = midrangeitems;
 
 //This is question 3 pound symbol \u00A3
+
+    var curarray = items.filter(function(curitems) {
+
+                            if (curitems.currency_code == "GBP") {
+
+                                    return curitems};
+                                  });
+
+    var curarray = items.map(function(itemprice){
+
+                                      return itemprice.price
+
+                                    });
+
 
     console.log(gbpitems);
     answer3 = gbpitems;
