0187aca850305c760a446a25f533675661dd054a
John Crocker <johnniecrocker@yahoo.com>
2014-10-01 14:47:39 -0400

    Set up html display and base js structure

---
+++ main.js
@@ -0,0 +1,62 @@
+//This is the javascript file for Assignment #6 - Array Questions
+
+
+var answer1,
+    answer2,
+    answer3,
+    answer4,
+    answer5,
+    answer6,
+    avgprice,             //average price of all items
+    eightitems,           //items with more than 8 materials
+    gbpitems,             //items denominated in pounds
+    midrangeitems,        //items priced between $14 and $18
+    selfmadeitems,        //items made by seller
+    wooditems             //items made of wood
+    ;
+
+//This is question 1
+
+    answer1 = avgprice;
+    console.log(avgprice);
+
+//This is question 2
+
+    console.log(midrangeitems);
+    answer2 = midrangeitems;
+
+//This is question 3 pound symbol \u00A3
+
+    console.log(gbpitems);
+    answer3 = gbpitems;
+
+//This is question 4
+
+    console.log(wooditems);
+    answer4 = wooditems;
+
+//This is question 5
+
+    console.log(eightitems);
+    answer5 = eightitems;
+
+//This is question 2
+
+    console.log(selfmadeitems);
+    answer6 = selfmadeitems;
+
+
+
+//This section displays the answers
+
+document.getElementById('answer1').innerHTML = answer1;
+
+document.getElementById('answer1').innerHTML = answer2;
+
+document.getElementById('answer1').innerHTML = answer3;
+
+document.getElementById('answer1').innerHTML = answer4;
+
+document.getElementById('answer1').innerHTML = answer5;
+
+document.getElementById('answer1').innerHTML = answer6;
